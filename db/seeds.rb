fb5498b583cc77b4cd32ebaf8de1911195cbc2cd
korezawa1005 <t.k.oshihiki@gmail.com>
2024-09-25 12:19:07 +0900

    検索機能(Ransack)練習

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require "faker"
+Faker::Config.locale = :ja
+
+# 1000件の初期データを投入
+1000.times do
+  User.create!(name: Faker::Name.name, age: rand(0..100))
+end
+
+puts "初期データの投入に成功しました！"
